4d8c493065b808b73a73437a421825ed46b2db9e
Mystery0 <mystery0dyl520@gmail.com>
2024-03-30 22:08:53 +0800

    fix: 调试自动更新版本号

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gitVersionCode=$(git rev-list --count HEAD)
 let gitVersionCode=gitVersionCode+1
-c="sed -E '4s/^version: ([0-9]+)\.([0-9]+)\.([0-9]+)\+[0-9]+/version: \1.\2.\3+$gitVersionCode/g' pubspec.yaml pubspec.yaml > temp.yaml"
+c="sed -E 's/^version: ([0-9]+)\.([0-9]+)\.([0-9]+)\+[0-9]+/version: \1.\2.\3+$gitVersionCode/g' pubspec.yaml pubspec.yaml > temp.yaml"
 eval $c
 rm -f pubspec.yaml
 mv temp.yaml pubspec.yaml
